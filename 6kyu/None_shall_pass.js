a13890345378c44883f2bc5ae3a4709d63a98ed8
Daniel Hogeland <dhogeland@Daniels-MacBook-Pro.local>
2017-10-12 13:47:14 -0500

    todays toy problems

---
+++ 6kyu/None_shall_pass.js
@@ -0,0 +1,6 @@
+// The black knight will be more persistent depending on the difficulty you specify.
+// Possible difficulties are easy, medium, hard, harder and impossible.
+// Default is easy.
+difficulty = 'easy';
+BlackKnight.chopOffArms();
+BlackKnight.chopOffLegs();
