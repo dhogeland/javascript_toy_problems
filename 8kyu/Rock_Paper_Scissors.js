c8adc2928ee8ac9528c8bb2897af9870770c689f
dhogeland <danielhogeland@gmail.com>
2018-04-05 18:49:20 -0500

    todays toy problems

---
+++ 8kyu/Rock_Paper_Scissors.js
@@ -0,0 +1,9 @@
+const rps = (p1, p2) => {
+  if (p1 == p2) {
+    return 'Draw!';
+  } if ((p1 == 'rock' && p2 == 'scissors') ||
+        (p1 == 'paper' && p2 == 'rock') ||
+        (p1 == 'scissors' && p2 == 'paper')) {
+    return 'Player 1 won!';
+  } return 'Player 2 won!';
+};
