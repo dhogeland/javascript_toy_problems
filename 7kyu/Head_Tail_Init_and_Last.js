2f4d3092da7b82a6e1f1dd940cb0a688dde031ab
dhogeland <danielhogeland@gmail.com>
2019-04-21 09:35:35 -0500

    todays toy problems

---
+++ 7kyu/Head_Tail_Init_and_Last.js
@@ -0,0 +1,12 @@
+function head(x) {
+  return x[0];
+}
+function tail(x) {
+  return x.slice(1);
+}
+function init(x) {
+  return x.slice(0, x.length - 1);
+}
+function last(x) {
+  return x[x.length - 1];
+}
