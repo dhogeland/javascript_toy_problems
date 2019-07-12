872ebdf0b8ed65249448b76734d8434d260e562d
dhogeland <danielhogeland@gmail.com>
2019-07-12 16:10:10 -0500

    todays toy problems

---
+++ 8kyu/Shopping_list.js
@@ -0,0 +1,11 @@
+let sandwiches = 4,
+    salads= 6,
+    wraps = 5,
+    frenchFries = 10,
+    price = {
+      sandwich: 8,
+      salad: 7,
+      wrap: 6.5,
+      frenchFries: 1.2
+      },
+    totalPrice = (sandwiches * price.sandwich) + (salads * price.salad) + (wraps * price.wrap) + (frenchFries * price.frenchFries);
